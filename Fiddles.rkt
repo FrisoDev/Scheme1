e3d8aa8c89a00e98716e3a8d7df817692aeeaa73
FrisoDev <frisodevstoker@gmail.com>
2018-11-01 14:47:15 +0100

    Recursieve fix

--- Fiddles.rkt
+++ Fiddles.rkt
@@ -8,7 +8,40 @@
 ;a
 (define (roteer-links lijst) (append (rest lijst) (list (first lijst))))
 
-(define (roteer-rechts lijst) (append (rest (reverse lijst)) (list (first (reverse lijst)))))
+(define (roteer-rechts lijst) (reverse(roteer-links (reverse lijst))))
+
+;b
+
+(define (roteer-links lijst keer)
+  (append (list-tail lijst keer) (take lijst keer)))
+
+(define (roteer-rechts lijst keer)
+  (roteer-links lijst (- ( length lijst) keer)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;(define (roteer-rechts lijst) (append (rest (reverse lijst)) (list (first (reverse lijst)))))
+;(define (roteer-rechts lijst) (define tsjil (reverse lijst))(roteer-links tsjil) (append (rest tsjil) (list (first tsjil))))
+
+
 
 ;(define (roteer-rechts lijst) (append
 ;  (list (first (reverse lijst)))
@@ -21,8 +54,22 @@
   (append (list-tail lijst keer) (take lijst keer)))
 
 (define (roteer-rechts lijst keer)
-  (append (list-tail (reverse lijst) (- (length lijst) keer) (take (reverse lijst) (-( length lijst) keer)))
+  (roteer-links lijst (- ( length lijst) keer)))
+
+
+
 
 ;(define (roteer-rechts lijst keer)
 ;  (append (list-tail lijst (- (length lijst) keer))
 ;(take lijst (- (length lijst) keer))))
+
+
+
+
+
+
+
+
+
+(define (roteer-rechts lijst keer)
+  (append (list-tail (reverse lijst) (- (length lijst) keer) (take (reverse lijst) (-( length lijst) keer)))
