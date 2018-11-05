3e35bf6e07f2c6586bfc67565295bd6ff1182fdc
FrisoDev <frisodevstoker@gmail.com>
2018-11-05 15:28:49 +0100

    Recursie 3

--- Recursie.rkt
+++ Recursie.rkt
@@ -1,9 +1,20 @@
+Recursie Scheme
+
+1.
+
 (define (aantal lst)
-  (if (empty? lst) '()
-  (cons (length (first lst))
-    (aantal (rest lst)))))
+  (if (empty? lst) 0
+    (+ 1 (aantal (rest lst)))))
 
 
+2.
 (define (totaal lst)
-  (if (empty? lst) '()
-    (+ (first lst) (totaal (rest list)))))
+  (if (empty? lst) 0
+    (+ (first lst) (totaal (rest lst)))))
+
+
+3.
+(define (in_lijst lijst letter)
+  (if (empty? lijst) #f
+    (if (eq? (first lijst)  letter) #t
+      (in_lijst (rest lijst) letter))))
